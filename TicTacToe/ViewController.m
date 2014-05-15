702c0e4e1f9b5e0e6dcca845f4dbb5ee8b761ceb
drw86312 <david.warner4@gmail.com>
2014-05-15 14:37:10 -0500

    sdfd

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -10,6 +10,20 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *myLabelOne;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelTwo;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelThree;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelFour;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelFive;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelSix;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelSeven;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelEight;
+@property (weak, nonatomic) IBOutlet UILabel *myLabelNine;
+
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
+@property CGPoint point;
+@property BOOL turn;
+
 @end
 
 @implementation ViewController
@@ -17,8 +31,55 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.turn = NO;
+
 }
+
+-(void)findLabelUsingPoint: (CGPoint) point
+{
+
+    switch (if (CGRectContainsPoint(Label, point)) {
+        case myLabelOne:
+            New label = my label one
+            break;
+
+        default:
+            break;
+    }
+
+            if Rect Label contains point {
+                set newlabel = label
+            }
+}
+
+-(IBAction)onLabelTapped:(UITapGestureRecognizer *) tap
+{
+    CGPoint point;
+    point = [tap locationInView:self.view];
+
+
+    if (self.turn) {
+        if (CGRectContainsPoint(self.myLabelOne.frame, point) && [self.myLabelOne.text isEqual: @" "]) {
+            self.myLabelOne.text = @"O";
+            self.myLabelOne.textColor = [UIColor redColor];
+            self.turn = !self.turn;
+            self.whichPlayerLabel.text = @"X";
+        }
+    } else {
+            if (CGRectContainsPoint(self.myLabelOne.frame, point) && [self.myLabelOne.text isEqual: @" "]) {
+                self.myLabelOne.text = @"X";
+                self.turn = !self.turn;
+                self.whichPlayerLabel.text = @"O";
+                }
+            }
+        }
+
+//    NSLog(@"%f %f", point.x, point.y);
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
