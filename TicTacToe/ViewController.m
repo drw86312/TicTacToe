4bbdbb6b3e3b09ebe306f09dfa453120bcc16661
ladiesman217 <bennyboy867@gmail.com>
2014-05-15 15:12:41 -0500

    Ben

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -24,6 +24,8 @@
 @property CGPoint point;
 @property BOOL turn;
 
+@property (weak, nonatomic) IBOutlet UILabel *clickedLabel;
+
 @end
 
 @implementation ViewController
@@ -35,51 +37,85 @@
 
 }
 
--(void)findLabelUsingPoint: (CGPoint) point
+-(void)findLabelUsingPoint:(CGPoint) pointNew
 {
-
-    switch (if (CGRectContainsPoint(Label, point)) {
-        case myLabelOne:
-            New label = my label one
-            break;
-
-        default:
-            break;
+    if (CGRectContainsPoint(_myLabelOne.frame, pointNew)) {
+        self.clickedLabel = self.myLabelOne;
+    } else if (CGRectContainsPoint(self.myLabelTwo.frame, pointNew)) {
+        self.clickedLabel = self.myLabelTwo;
+    } else if (CGRectContainsPoint(self.myLabelThree.frame, pointNew)) {
+        self.clickedLabel = self.myLabelThree;
+    } else if (CGRectContainsPoint(self.myLabelFour.frame, pointNew)) {
+        self.clickedLabel = self.myLabelFour;
+    }else if (CGRectContainsPoint(self.myLabelFive.frame, pointNew)) {
+        self.clickedLabel = self.myLabelFive;
+    }else if (CGRectContainsPoint(self.myLabelSix.frame, pointNew)) {
+        self.clickedLabel = self.myLabelSix;
+    }else if (CGRectContainsPoint(self.myLabelSeven.frame, pointNew)) {
+        self.clickedLabel = self.myLabelSeven;
+    }else if (CGRectContainsPoint(self.myLabelEight.frame, pointNew)) {
+        self.clickedLabel = self.myLabelEight;
+    }else if (CGRectContainsPoint(self.myLabelNine.frame, pointNew)) {
+        self.clickedLabel = self.myLabelNine;
     }
+}
 
-            if Rect Label contains point {
-                set newlabel = label
-            }
+-(void)xIsWinner {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"X won" message:@"X won" delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:nil, nil ];
+    [alert show];
+}
+
+-(void)yIsWinner {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Y won" message:@"Y won" delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:nil, nil ];
+    [alert show];
 }
 
 -(IBAction)onLabelTapped:(UITapGestureRecognizer *) tap
 {
-    CGPoint point;
-    point = [tap locationInView:self.view];
 
+    self.point = [tap locationInView:self.view];
 
+    [self findLabelUsingPoint:self.point];
+    // if BOOL = YES for Turn, set in viewDidLoad
     if (self.turn) {
-        if (CGRectContainsPoint(self.myLabelOne.frame, point) && [self.myLabelOne.text isEqual: @" "]) {
-            self.myLabelOne.text = @"O";
-            self.myLabelOne.textColor = [UIColor redColor];
+        if (CGRectContainsPoint(self.clickedLabel.frame, self.point) && [self.clickedLabel.text isEqual: @" "]) {
+            self.clickedLabel.text = @"O";
+            self.clickedLabel.textColor = [UIColor redColor];
             self.turn = !self.turn;
             self.whichPlayerLabel.text = @"X";
-        }
+            }
+// Else other turn
     } else {
-            if (CGRectContainsPoint(self.myLabelOne.frame, point) && [self.myLabelOne.text isEqual: @" "]) {
-                self.myLabelOne.text = @"X";
+            if (CGRectContainsPoint(self.clickedLabel.frame, self.point) && [self.clickedLabel.text isEqual: @" "]) {
+                self.clickedLabel.text = @"X";
+                self.clickedLabel.textColor = [UIColor blueColor];
                 self.turn = !self.turn;
                 self.whichPlayerLabel.text = @"O";
                 }
             }
-        }
+
+
+//Checking if User Won
+
+    if ([self.myLabelOne  isEqual: @"X"] && [self.myLabelTwo  isEqual: @"X"] && [self.myLabelThree  isEqual: @"X"]) {
+
+        [self xIsWinner];
+    }    else if ([self.myLabelOne  isEqual: @"X"] && [self.myLabelTwo  isEqual: @"X"] && [self.myLabelThree  isEqual: @"X"]) {
+
+        [self xIsWinner];
+
+    }  else if ([self.myLabelOne  isEqual: @"X"] && [self.myLabelTwo  isEqual: @"X"] && [self.myLabelThree  isEqual: @"X"]) {
+
+        [self xIsWinner];
+    }
+
 
 //    NSLog(@"%f %f", point.x, point.y);
 
 
 
 
-
+}
 
 - (void)didReceiveMemoryWarning
 {
